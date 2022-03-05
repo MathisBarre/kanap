3101d9999b822c143b7fff4edc2b057ab848b74a
Mathis Barré <m.barre@beapp.fr>
2022-03-05 12:55:28 +0100

    feat: Cart UI

---
+++ components/pages/cart/CartListItem.tsx
@@ -0,0 +1,41 @@
+import { CartItem } from "../../../domain/cart";
+
+interface CartListItemProps {
+  cartItem: CartItem
+}
+
+export function CartListItem({ cartItem }: CartListItemProps) {
+  return (
+    <article
+      className="cart__item"
+      data-id="{product-ID}"
+      data-color="{product-color}"
+    >
+      <div className="cart__item__img">
+        <img src={cartItem.product.imageUrl} alt="Photographie d'un canapé" />
+      </div>
+      <div className="cart__item__content">
+        <div className="cart__item__content__description">
+          <h2>{cartItem.product.name}</h2>
+          <p>{cartItem.color}</p>
+          <p>{cartItem.product.price} €</p>
+        </div>
+        <div className="cart__item__content__settings">
+          <div className="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input
+              type="number"
+              className="itemQuantity"
+              name="itemQuantity"
+              min={1}
+              max={100}
+            />
+          </div>
+          <div className="cart__item__content__settings__delete">
+            <p className="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
---
+++ pages/cart.tsx
@@ -0,0 +1,71 @@
+import { cartStorageAdaptater } from "../adaptaters/storageAdaptater";
+import { CartStorage } from "../application/ports";
+import { CartListItem } from "../components/pages/cart/CartListItem";
+import { Cart, CartItem } from "../domain/cart";
+
+export default function CartPage() {
+  const cartStorage: CartStorage = cartStorageAdaptater;
+  const cart: Cart = cartStorage.getCart();
+
+  return (
+    <main className="limitedWidthBlockContainer">
+      <div className="limitedWidthBlock" id="limitedWidthBlock">
+        <div className="cartAndFormContainer" id="cartAndFormContainer">
+          <h1>Votre panier</h1>
+          <section className="cart">
+            <section id="cart__items">
+              {cart.map((cartItem: CartItem) => {
+                return (
+                  <CartListItem
+                    cartItem={cartItem}
+                    key={`${cartItem.product.id}.${cartItem.product.colors}`}
+                  />
+                );
+              })}
+            </section>
+            <div className="cart__price">
+              <p>
+                Total (<span id="totalQuantity">{/* 2 */}</span> articles) :{" "}
+                <span id="totalPrice">{/* 84,00 */}</span> €
+              </p>
+            </div>
+            <div className="cart__order">
+              <form method="get" className="cart__order__form">
+                <div className="cart__order__form__question">
+                  <label htmlFor="firstName">Prénom: </label>
+                  <input type="text" name="firstName" id="firstName" required />
+                  <p id="firstNameErrorMsg">
+                    {/* ci est un message d'erreur */}
+                  </p>
+                </div>
+                <div className="cart__order__form__question">
+                  <label htmlFor="lastName">Nom: </label>
+                  <input type="text" name="lastName" id="lastName" required />
+                  <p id="lastNameErrorMsg" />
+                </div>
+                <div className="cart__order__form__question">
+                  <label htmlFor="address">Adresse: </label>
+                  <input type="text" name="address" id="address" required />
+                  <p id="addressErrorMsg" />
+                </div>
+                <div className="cart__order__form__question">
+                  <label htmlFor="city">Ville: </label>
+                  <input type="text" name="city" id="city" required />
+                  <p id="cityErrorMsg" />
+                </div>
+                <div className="cart__order__form__question">
+                  <label htmlFor="email">Email: </label>
+                  <input type="email" name="email" id="email" required />
+                  <p id="emailErrorMsg" />
+                </div>
+                <div className="cart__order__form__submit">
+                  <input type="submit" defaultValue="Commander !" id="order" />
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+}
